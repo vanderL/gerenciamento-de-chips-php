0d1abe9a9c7bb9f1e6ac17bd62c584854f3121e2
Vander Lima <vnd.vander@gmail.com>
2016-06-01 20:16:53 -0300

    retirada da conexão privada.

--- php/conexao.php
+++ php/conexao.php
@@ -1,4 +1,4 @@
 <?php
-/*$conexao = mysqli_connect('localhost', 'root', 'root', 'mydb');*/
-							#IP BANCO 		Usuario		Senha 		Nome Database
-$conexao = mysqli_connect('192.168.0.49', 'root', 'desenv@apta', 'db_monitoramento');
+$conexao = mysqli_connect('localhost', 'root', 'root', 'mydb');
+			   #IP BANCO 		Usuario		Senha 		Nome Database
+//$conexao = mysqli_connect('privado', 'privado', 'privado', 'privado');
